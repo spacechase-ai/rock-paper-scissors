e8897863ad9606bf352d0dadea099d15edda0716
spacechase-ai <nicholaspfisher21@gmail.com>
2022-01-22 20:26:04 -0500

    Fixed several bugs, added 5x loop and endig game.

--- script.js
+++ script.js
@@ -10,42 +10,61 @@ let playerSelection = prompt(
 );
 
 //Validates input and makes case insensitive
-function validateForm() {
+function checkNull() {
 	if (playerSelection == null || playerSelection == "") {
 		alert("Please enter your choice!");
 	} else {
 	}
 }
-validateForm(playerSelection);
+checkNull(playerSelection);
 playerSelection = playerSelection.toLowerCase();
 
-//playRound compares values
+//playRound compares values and sets base scores to 0
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound() {
 	if (playerSelection == computerSelection) {
 		alert("It's a tie!");
 	} else if (playerSelection == "paper" && computerSelection == "rock") {
 		alert("You win! Paper beats rock.");
+		playerScore++;
 	} else if (playerSelection == "rock" && computerSelection == "scissors") {
 		alert("You win! Rock beats scissors.");
+		playerScore++;
 	} else if (playerSelection == "scissors" && computerSelection == "paper") {
 		alert("You win! Scissors beats paper.");
+		playerScore++;
 	} else if (playerSelection == "rock" && computerSelection == "paper") {
 		alert("You lose! Paper beats rock.");
+		computerScore++;
 	} else if (playerSelection == "scissors" && computerSelection == "rock") {
 		alert("You lose! Rock beats scissors.");
+		computerScore++;
 	} else if (playerSelection == "paper" && computerSelection == "scissors") {
 		alert("You win! Paper beats scissors.");
+		computerScore++;
 	} else {
 	}
 }
 
-playRound(playerSelection, computerSelection);
-
-confirm ("Play again?") {
-    if (true) {
-        playerSelection
-    }
+//Loop will execute a 5 round game
+for (let i = 0; i < 5; i++) {
+	playRound(playerSelection, computerSelection);
+	playerSelection = prompt("Please choose your fate... Rock, Paper, Scissors!");
+	checkNull(playerSelection);
+	playerSelection = playerSelection.toLowerCase();
+	computerOptions[Math.floor(Math.random() * computerOptions.length)];
 }
 
-//sunction playRount will take playerSelection and computerSelection to see who wins
-//Return string declaring player win or lose
+function winner() {
+	if (playerScore > computerScore) {
+		alert("You win the game!");
+	} else if (playerScore < computerScore) {
+		alert("You lose the game!");
+	} else {
+		alert("Tie game!");
+	}
+}
+
+winner(playerScore, computerScore);
